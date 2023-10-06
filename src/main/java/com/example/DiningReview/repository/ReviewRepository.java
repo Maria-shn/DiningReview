2c287b6de99e156ebc1a33fa88e899bcc2302101
Maria <maria.shn.es@gmail.com>
2023-10-06 18:04:42 +0200

    removed application properties temporarily

--- src/main/java/com/example/DiningReview/repository/ReviewRepository.java
+++ src/main/java/com/example/DiningReview/repository/ReviewRepository.java
@@ -5,11 +5,9 @@ import org.springframework.data.repository.CrudRepository;
 import com.example.DiningReview.entities.Review;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
 
 public interface ReviewRepository extends CrudRepository<Review, Long> {
     List<Review> findReviewsByRestaurantIdAndUserName(Long restaurantId, String userName);
